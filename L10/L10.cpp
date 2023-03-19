fe2e54fed16b627c0937e4530b30fc281f5c7e52
JJH921104 <JJH921104@DESKTOP-9G3J8IJ>
2023-03-19 22:02:26 +0800

    加入專案檔案。

---
+++ L10/L10.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream> 
+#include <algorithm>  
+using namespace std;
+int main() {
+    //ios::sync_with_stdio(0);
+    //cin.tie(0);
+    int a, b;
+    while (cin >> a >> b) {
+        cout <<__gcd(a, b) << "\n";
+    }
+    return 0;
+}
